f3aea32a0e982baf01945ed7e6618e4921392bef
wukunyang <wukunyang2001@gmail.com>
2018-09-18 13:55:44 +0200

    update init() and simpify onDraw()

--- app/src/main/java/com/example/snake/SnakeGridView.java
+++ app/src/main/java/com/example/snake/SnakeGridView.java
@@ -12,56 +12,59 @@ public class SnakeGridView extends View {
 
     private final int GRID_NUM = 25;
 
+    private float gridSize;
+    private Paint paintFill = new Paint();
+    private Paint paintStroke = new Paint();
+    private int[][] gridColor;
+
     public SnakeGridView(Context context) {
         super(context);
         init();
     }
-
     public SnakeGridView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
     }
-
     public SnakeGridView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
-
     private void init(){
+        gridColor = new int[GRID_NUM][GRID_NUM];
+        for(int i = 0; i < GRID_NUM; i++){
+            for(int j = 0; j < GRID_NUM; j++) {
+                gridColor[i][j] = Color.WHITE;
+            }
+        }
+        paintFill.setStyle(Paint.Style.FILL);
+        paintStroke.setColor(Color.GRAY);
+        paintStroke.setStyle(Paint.Style.STROKE);
     }
 
-    private int canvasSize;
-    private float gridSize;
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        canvasSize = getMeasuredWidth();
+        int canvasSize = getMeasuredWidth();
         gridSize = (float) canvasSize / GRID_NUM;
         setMeasuredDimension(canvasSize, canvasSize);
     }
 
-    private Paint paintFill = new Paint();
-    private Paint paintStroke = new Paint();
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        paintFill.setColor(Color.GRAY);
-        paintFill.setStyle(Paint.Style.FILL);
-
-        paintStroke.setColor(Color.BLACK);
-        paintStroke.setStyle(Paint.Style.STROKE);
-
         for(int i = 0; i < GRID_NUM; i++){
             for(int j = 0; j < GRID_NUM; j++){
                 float top = i * gridSize;
                 float bottom = top + gridSize;
                 float left = j* gridSize;
                 float right = left + gridSize;
+                paintFill.setColor(gridColor[i][j]);
                 canvas.drawRect(left, top, right, bottom, paintFill);
                 canvas.drawRect(left, top, right, bottom, paintStroke);
             }
         }
     }
+
+    
 
 }
