b78c824013eba32a04c1cc9c46624402e758e61d
wukunyang <wukunyang2001@gmail.com>
2018-09-20 19:52:23 +0200

    add settings in menu

--- app/src/main/java/com/example/snake/MainActivity.java
+++ app/src/main/java/com/example/snake/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.snake;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private final int DIRECTION_DOWN = -100;
     private final int DIRECTION_LEFT = 101;
     private final int DIRECTION_RIGHT = -101;
+
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +43,20 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button:
                 snakeGridView.start();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+        }
+        return false;
     }
 }
